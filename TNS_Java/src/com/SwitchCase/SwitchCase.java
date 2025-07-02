0d2337a00b4cbcc3507c99f82361623326233a41
SiddhantPangul <164644428+SiddhantPangul17@users.noreply.github.com>
2025-07-02 10:32:15 +0530

    Pushed Assignment 2

---
+++ TNS_Java/src/com/SwitchCase/SwitchCase.java
@@ -0,0 +1,32 @@
+package com.SwitchCase;
+
+public class SwitchCase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char x ='D';
+		
+		switch(x)
+		{
+		case 'l':
+			System.out.println("This code contains letters");
+			break;
+			
+		case 'd':
+			System.out.println("This code contains letters");
+			break;
+		
+		case 's':
+			System.out.println("This code contains letters");
+			break;
+		
+		case 'w':
+			System.out.println("This code contains letters");
+			break;
+			
+		default:
+			System.out.println("This code does not contains letters");
+		}
+	}
+
+}
---
+++ TNS_Java/src/dayone/FirstProgram.java
@@ -0,0 +1,11 @@
+package dayone;
+
+public class FirstProgram {
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World");
+		System.out.println(10+5);
+	}
+
+}
